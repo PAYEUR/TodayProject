f9e572c0279502e01ee995a467c1203f9be8362c
PAYEUR <jeanbaptistepayeur@gmail.com>
2016-01-03 20:58:05 +0100

    linked event with image

--- today/models.py
+++ today/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from swingtime import models as swingtime
-# Create your models here.
+from swingtime.models import Event
+
+
+class Image(models.Model):
+    """ Single table that contains images either from download from existing db
+    """
+    field = models.ImageField()
+
+
+class EventWithImage(models.Model):
+    """ swingtime.Event + Image so that an event in todayProject is necessarily linked to an image
+    """
+    event = models.ForeignKey(Event)
+    image = models.ForeignKey(Image)
+
+    def __str__(self):
+        return self.title
