ec7a5c9661dc685e104af50f34f2b0549b063187
PAYEUR <jeanbaptistepayeur@gmail.com>
2016-01-09 17:14:21 +0100

    starting using bootstrap

--- today/views.py
+++ today/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import NameForm
 
 
 def index(request):
     return render(request, 'today/home.html')
+
+
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/today/')
+    else:
+        form = NameForm()
+
+    return render(request, 'today/form.html', {'form': form})
 
