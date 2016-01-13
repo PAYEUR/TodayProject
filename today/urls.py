4696b84c7b818e9add2fab45fb51b35db7e05ad7
PAYEUR <jeanbaptistepayeur@gmail.com>
2016-01-13 21:25:06 +0100

    home wpage structure quite ok

--- today/urls.py
+++ today/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^base$', views.base, name="base"),
-    url(r'^base2$', views.base2, name="base2"),
     url(r'^inscription$', views.get_name, name="get_name"),
     url(r'^evenement$', views.get_event, name="get_event")
     ]
