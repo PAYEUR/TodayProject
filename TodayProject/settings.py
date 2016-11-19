666d8428f8a82fdad154c9fd0c999d7174286e59
PAYEUR <jeanbaptistepayeur@gmail.com>
2016-11-19 11:52:41 +0100

    Adding a settings file to run locally with pycharm

---
+++ TodayProject/settings.py
@@ -0,0 +1,11 @@
+# This file has only been written in order to run easily pycharm
+
+from localdev_settings import *
+
+
+#launch it with following code:
+#  runserver --settings TodayProject.local_dev_paris_settings 127.0.0.1:8000
+
+#SITE_ID = 2
+
+#ALLOWED_HOSTS = ['127.0.0.1']
