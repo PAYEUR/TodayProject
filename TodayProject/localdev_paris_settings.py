cd561eeb237dd46372d6d41525e5beda686f7e1b
PAYEUR <jeanbaptistepayeur@gmail.com>
2016-11-24 09:01:31 +0100

    Readded wgsi.py. Removed settings.py

--- TodayProject/localdev_paris_settings.py
+++ TodayProject/localdev_paris_settings.py
@@ -2,7 +2,7 @@ from localdev_settings import *
 
 
 #launch it with following code:
-#  runserver --settings TodayProject.local_dev_paris_settings 127.0.0.1:8000
+#  runserver --settings TodayProject.localdev_paris_settings 127.0.0.1:8000
 
 SITE_ID = 2
 
