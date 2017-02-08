375d8a33927cdbf8e812b16ca18cb35a64b0e7b5
= <=>
2017-02-08 18:09:16 +0100

    Trying to implement one single url

--- topic/urls.py
+++ topic/urls.py
@@ -14,15 +14,16 @@ urlpatterns = [
 
     url(r'^categorie(?P<event_type_id_string>.+)/(?P<year>\d{4})/(?P<month>0?[1-9]|1[012])/(?P<day>[0-3]?\d)/$',
         views.single_day_event_type_list, name="single_day_event_type_list"),
-
-    url(r'^categorie(?P<event_type_id_string>.+)/(?P<year>\d{4})/(?P<month>0?[1-9]|1[012])/(?P<day>[0-3]?\d)/(?P<start_hour_string>.+)-(?P<end_hour_string>.+)$',
-        views.single_time_event_type_list, name="single_time_event_type_list"),
-
     # this one is not really usefull
     url(r'^toutes-categories/(?P<year>\d{4})/(?P<month>0?[1-9]|1[012])/(?P<day>[0-3]?\d)/$', views.daily_events,
         name="daily_events"),
     # neither this one as event_type_string has been created
     url(r'^categorie(?P<event_type_id_string>[0-9]+)/$', views.event_type_coming_days, name="event_type_coming_days"),
 
+    url(r'^categorie(?P<event_type_id_string>.+)/(?P<year>\d{4})/(?P<month>0?[1-9]|1[012])/(?P<day>[0-3]?\d)/(?P<start_hour_string>.+)-(?P<end_hour_string>.+)$',
+        views.single_time_event_type_list, name="occurence_list"),
 
+    #in the idea, one should have a single string like that
+    url(r'^categorie(?P<event_type_id_string>.+)/du_(?P<start_year>\d{4})-(?P<start_month>0?[1-9]|1[012])-(?P<start_day>[0-3]?\d)_ a_(?P<start_hour_string>.+)/au_(?P<end_year>\d{4})-(?P<end_month>0?[1-9]|1[012])-(?P<end_day>[0-3]?\d)_ a_(?P<end_hour_string>.+)$',
+        views.single_time_event_type_list, name="single_time_event_type_list"),
     ]
