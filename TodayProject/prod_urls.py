8ce531fe001c879da98d6c61991a3c47523ef243
PAYEUR <jeanbaptistepayeur@gmail.com>
2018-11-20 17:39:05 +0100

    update events from /update-events/

--- TodayProject/prod_urls.py
+++ TodayProject/prod_urls.py
@@ -6,10 +6,10 @@ from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'^update-events/', include('update_external_events.urls')),
     url(r'^', include('core.urls')),
     url(r'^', include('crud.urls')),
     url(r'^connexion/', include('connection.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^(?P<city_slug>[\w-]+)/', include('location.urls')),
-    url(r'update-events', include('update_external_events.urls'))
     ]
