340bb24c2bbb9d034ef73e4859ea5a5136a5d436
PAYEUR <jeanbaptistepayeur@gmail.com>
2018-12-19 22:52:53 +0100

    Error while calling sitemaps in core/urls

---
+++ core/sitemaps.py
@@ -0,0 +1,38 @@
+from django.contrib.sitemaps import Sitemap, GenericSitemap
+from django.core.urlresolvers import reverse
+
+from topic.models import Occurrence
+
+
+class DynamicSitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'weekly'
+
+    def items(self):
+        return Occurrence.objects.all()
+
+
+class StaticSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'yearly'
+
+    def items(self):
+        return ['index',
+                'contact',
+                'team',
+                'CGU',
+                'cookies',
+                'help_us',
+                'tutorial',
+                'presentation',
+                'charte',
+                'explain_categories']
+
+    def location(self, item):
+        return reverse(item)
+
+
+def sitemaps():
+    return {'dynamic': DynamicSitemap,
+            'static': StaticSitemap,
+            }
--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,13 @@
 from django.conf.urls import url
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 
 from .views import *
-from django.contrib.sitemaps import GenericSitemap
-from django.contrib.sitemaps.views import sitemap
-from topic.models import Occurrence
-
-sitemap_dict = {
-    'queryset': Occurrence.objects.all(),
-    'date_field': 'start_time',
-}
+from .sitemaps import sitemaps
 
 
 app_name = 'core'
 urlpatterns = [
-    url(r'^sitemap\.xml$', sitemap,
-        {'sitemaps': {'enjoytoday': GenericSitemap(sitemap_dict, priority=0.6)}},
-        name='django.contrib.sitemaps.views.sitemap'),
     url(r'^$', IndexView.as_view(), name="index"),
     url(r'^contact$', ContactView.as_view(), name="contact"),
     url(r'^l-equipe-enjoytoday', TeamView.as_view(), name="team"),
@@ -26,4 +18,7 @@ urlpatterns = [
     url(r'^presentation-du-projet', PresentationView.as_view(), name="presentation"),
     url(r'^charte-post-d-evenement', CharteView.as_view(), name="charte"),
     url(r'^presentation-des-categories', ExplainCategoriesView.as_view(), name="explain_categories"),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name="core/robots.txt", content_type='text/plain')),
+
     ]
